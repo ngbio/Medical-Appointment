b1952f8b76886936404259202de6680f4cd636bf
trang ngo <kieutrangg30805@gmail.com>
2026-04-26 04:44:55 +0700

    prepare seed data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,18 @@ else
     echo "Skipping collectstatic..."
 fi
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
+
+python data.py
+
 # Run the appropriate service based on CELERY_COMMAND env var
 if [ "$CELERY_COMMAND" = "worker" ]; then
     echo "Starting Celery Worker..."
@@ -25,7 +37,6 @@ elif [ "$CELERY_COMMAND" = "beat" ]; then
     exec celery -A src beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
     echo "Starting Django Server..."
-    # exec python manage.py runserver 0.0.0.0:8000
-    exec gunicorn src.wsgi:application --bind 0.0.0.0:8000
+    exec python manage.py runserver 0.0.0.0:8000
 fi
 
